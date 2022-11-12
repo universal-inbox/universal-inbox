b304182041e69ee48addc83e53cfe60895b4ecc6
David Rousselie <david@unified-inbox.app>
2022-11-12 15:29:43 +0100

    feat(web): Add remote API call status notifications

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,14 +1,23 @@
 extern crate console_error_panic_hook;
 
 use cfg_if::cfg_if;
+use log::{info, Level};
 use std::panic;
 use universal_inbox_web::app;
 
 cfg_if! {
+    if #[cfg(debug_assertions)] {
+        const LOG_LEVEL: Level = Level::Trace;
+    } else {
+        const LOG_LEVEL: Level = Level::Info;
+    }
+}
+
+cfg_if! {
     if #[cfg(feature = "console_log")] {
         fn init_log() {
-            use log::Level;
-            console_log::init_with_level(Level::Trace).expect("error initializing log");
+            console_log::init_with_level(LOG_LEVEL).expect("error initializing log");
+            info!("Log level set to {}", LOG_LEVEL.to_string());
         }
     } else {
         fn init_log() {}
