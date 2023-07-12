000a844ac72a5eb95330a2413f3a5f096d28d0b0
David Rousselie <david@rousselie.name>
2023-07-12 21:00:52 +0200

    style: Add more details to the notification list

--- web/src/components/notifications_list.rs
+++ web/src/components/notifications_list.rs
@@ -1,13 +1,27 @@
 use dioxus::{events::MouseEvent, prelude::*};
 use dioxus_free_icons::{
-    icons::bs_icons::{
-        BsBellSlash, BsCalendar2Check, BsCheck2, BsClockHistory, BsLink45deg, BsTrash,
+    icons::{
+        bs_icons::{
+            BsArrowRepeat, BsBellSlash, BsCalendar2Check, BsCardChecklist, BsChat, BsCheck2,
+            BsCheckCircle, BsClockHistory, BsLink45deg, BsRecordCircle, BsTrash,
+        },
+        io_icons::IoGitPullRequest,
     },
     Icon,
 };
 use fermi::UseAtomRef;
+use http::Uri;
 
-use universal_inbox::notification::{NotificationMetadata, NotificationWithTask};
+use log::debug;
+use universal_inbox::{
+    notification::{
+        integrations::github::GithubNotification, NotificationMetadata, NotificationWithTask,
+    },
+    task::{
+        integrations::todoist::{TodoistItem, TodoistItemPriority},
+        TaskMetadata,
+    },
+};
 
 use crate::{
     components::icons::{github, todoist},
@@ -164,13 +178,6 @@ fn notification<'a>(
             }
         },
     );
-    let button_style = use_memo(cx, (selected,), |(selected,)| {
-        if selected {
-            "visible"
-        } else {
-            "invisible group-hover:visible"
-        }
-    });
 
     cx.render(rsx!(
         tr {
@@ -182,13 +189,74 @@ fn notification<'a>(
                 }
             },
 
-            self::notification_display { notif: notif }
+            self::notification_display { notif: notif, selected: *selected, children }
+        }
+    ))
+}
 
-            td {
-                class: "p-2 rounded-none",
+#[inline_props]
+fn notification_display<'a>(
+    cx: Scope,
+    notif: &'a NotificationWithTask,
+    selected: bool,
+    children: Element<'a>,
+) -> Element {
+    let icon = match notif.metadata {
+        NotificationMetadata::Github(_) => cx.render(rsx!(self::github { class: "h-5 w-5" })),
+        NotificationMetadata::Todoist => cx.render(rsx!(self::todoist { class: "h-5 w-5" })),
+    };
+    let button_style = use_memo(cx, (selected,), |(selected,)| {
+        if selected {
+            "swap-active"
+        } else {
+            "group-hover:swap-active"
+        }
+    });
+    let notif_updated_at = use_memo(cx, &(notif.updated_at,), |(updated_at,)| {
+        updated_at.format("%Y-%m-%d %H:%M")
+    });
+
+    cx.render(rsx!(
+        td {
+             class: "px-2 py-0 rounded-none",
+             div { class: "flex justify-center", icon } }
+        td {
+            class: "px-2 py-0",
+
+            match &notif.metadata {
+                NotificationMetadata::Github(github_notification) => rsx!(
+                    self::github_notification_display {
+                        notif: notif,
+                        github_notification: github_notification.clone(),
+                    }
+                ),
+                NotificationMetadata::Todoist => rsx!(
+                    if let Some(task) = &notif.task {
+                        match &task.metadata {
+                            TaskMetadata::Todoist(todoist_task) => rsx!(
+                                self::todoist_notification_display {
+                                    notif: notif,
+                                    todoist_task: todoist_task.clone(),
+                                }
+                            )
+                        }
+                    } else {
+                        rsx!(self::default_notification_display { notif: notif })
+                    }
+                )
+            }
+        }
+        td {
+            class: "px-2 py-0 rounded-none flex flex-wrap items-center justify-end",
+            div {
+                class: "swap {button_style}",
                 div {
-                    class: "{button_style} flex justify-end",
+                    class: "swap-on flex items-center justify-end",
                     children
+                }
+                div {
+                    class: "swap-off text-xs text-gray-400 flex items-center justify-end group-hover:invisible",
+                    "{notif_updated_at}"
                 }
             }
         }
@@ -196,23 +264,162 @@ fn notification<'a>(
 }
 
 #[inline_props]
-fn notification_display<'a>(cx: Scope, notif: &'a NotificationWithTask) -> Element {
-    let icon = match notif.metadata {
-        NotificationMetadata::Github(_) => cx.render(rsx!(self::github { class: "h-5 w-5" })),
-        NotificationMetadata::Todoist => cx.render(rsx!(self::todoist { class: "h-5 w-5" })),
+fn default_notification_display<'a>(cx: Scope, notif: &'a NotificationWithTask) -> Element {
+    if let Some(link) = &notif.source_html_url {
+        cx.render(rsx!(a { href: "{link}", target: "_blank", "{notif.title}" }))
+    } else {
+        cx.render(rsx!("{notif.title}"))
+    }
+}
+
+#[inline_props]
+fn github_notification_display<'a>(
+    cx: Scope,
+    notif: &'a NotificationWithTask,
+    github_notification: GithubNotification,
+) -> Element {
+    let github_notification_id = extract_github_notification_id(&notif.source_html_url);
+    let notification_source_url = notif
+        .source_html_url
+        .as_ref()
+        .map(|url| url.to_string())
+        .unwrap_or_else(|| {
+            debug!(
+                "No source url for notification with Github notification reason: {:?}",
+                github_notification.reason
+            );
+            match github_notification.subject.r#type.as_str() {
+                // There is no enough information in the notification to link to the source
+                "CheckSuite" => format!("{}/actions", github_notification.repository.html_url),
+                "Discussion" => format!(
+                    "{}/discussions?{}",
+                    github_notification.repository.html_url,
+                    serde_urlencoded::to_string([("discussions_q", notif.title.clone())])
+                        .unwrap_or_default()
+                ),
+                _ => github_notification.repository.html_url.to_string(),
+            }
+        });
+    let type_icon = match github_notification.subject.r#type.as_str() {
+        "PullRequest" => cx.render(rsx!(Icon {
+            class: "h-5 w-5",
+            icon: IoGitPullRequest
+        })),
+        "Issue" => cx.render(rsx!(Icon {
+            class: "h-5 w-5",
+            icon: BsRecordCircle
+        })),
+        "Discussion" => cx.render(rsx!(Icon {
+            class: "h-5 w-5",
+            icon: BsChat
+        })),
+        "CheckSuite" => cx.render(rsx!(Icon {
+            class: "h-5 w-5",
+            icon: BsCheckCircle
+        })),
+        _ => None,
     };
 
     cx.render(rsx!(
-        td {
-             class: "p-2 rounded-none",
-             div { class: "flex justify-center", icon } }
-        td {
-            class: "p-2",
+        div {
+            class: "flex items-center gap-2",
 
-            if let Some(link) = &notif.source_html_url {
-                rsx!(a { href: "{link}", target: "_blank", "{notif.title}" })
-            } else {
-                rsx!("{notif.title}")
+            type_icon
+
+            div {
+                class: "flex flex-col grow",
+
+                a {
+                    href: "{notification_source_url}",
+                    target: "_blank",
+                    "{notif.title}"
+                }
+                div {
+                    class: "flex gap-2",
+
+                    a {
+                        class: "text-xs text-gray-400",
+                        href: "{github_notification.repository.html_url.clone()}",
+                        target: "_blank",
+                        "{github_notification.repository.full_name}"
+                    }
+
+                    a {
+                        class: "text-xs text-gray-400",
+                        href: "{notification_source_url}",
+                        target: "_blank",
+                        if let Some(github_notification_id) = github_notification_id {
+                            rsx!("#{github_notification_id} ")
+                        }
+                        "({github_notification.reason})"
+                    }
+                }
+            }
+        }
+    ))
+}
+
+fn extract_github_notification_id(url: &Option<Uri>) -> Option<String> {
+    let Some(url) = url else { return None };
+    let mut url_parts = url.path().split('/').collect::<Vec<_>>();
+    let id = url_parts.pop()?;
+    Some(id.to_string())
+}
+
+#[inline_props]
+fn todoist_notification_display<'a>(
+    cx: Scope,
+    notif: &'a NotificationWithTask,
+    todoist_task: TodoistItem,
+) -> Element {
+    let notification_source_url = notif
+        .source_html_url
+        .as_ref()
+        .map(|url| url.to_string())
+        .unwrap_or_else(|| "https://todoist.com/app".to_string());
+    let task_icon_style = match todoist_task.priority {
+        TodoistItemPriority::P1 => "",
+        TodoistItemPriority::P2 => "text-yellow-500",
+        TodoistItemPriority::P3 => "text-orange-500",
+        TodoistItemPriority::P4 => "text-red-500",
+    };
+
+    cx.render(rsx!(
+        div {
+            class: "flex items-center gap-2",
+
+            Icon { class: "h-5 w-5 {task_icon_style}", icon: BsCardChecklist }
+
+             div {
+                class: "flex flex-col grow",
+
+                a {
+                    href: "{notification_source_url}",
+                    target: "_blank",
+                    "{notif.title}"
+                }
+                div {
+                    class: "flex gap-2",
+
+                    if let Some(due) = &todoist_task.due {
+                        rsx!(div {
+                            class: "flex items-center text-xs text-gray-400 gap-1",
+
+                            Icon { class: "h-3 w-3", icon: BsCalendar2Check }
+                            "{due.date}"
+                            if due.is_recurring {
+                                rsx!(Icon { class: "h-3 w-3", icon: BsArrowRepeat })
+                            }
+                        })
+                    }
+
+                    div {
+                        class: "flex gap-2",
+                        for label in &todoist_task.labels {
+                            rsx!(span { class: "text-xs text-gray-400", "@{label}" })
+                        }
+                    }
+                }
             }
         }
     ))
