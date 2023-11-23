de86a7c7b65d1d747f994eee183b0706ff6a95a9
David Rousselie <david@rousselie.name>
2023-11-23 21:33:58 +0100

    refactor: Move GoogleMail & Todoist list item in their own module

---
+++ web/src/components/integrations/google_mail/notification.rs
@@ -0,0 +1,76 @@
+#![allow(non_snake_case)]
+
+use std::collections::HashSet;
+
+use dioxus::prelude::*;
+use dioxus_free_icons::{
+    icons::bs_icons::{BsExclamationCircle, BsStar},
+    Icon,
+};
+
+use universal_inbox::notification::{
+    integrations::google_mail::{
+        GoogleMailThread, MessageSelection, GOOGLE_MAIL_IMPORTANT_LABEL, GOOGLE_MAIL_STARRED_LABEL,
+    },
+    NotificationWithTask,
+};
+
+use crate::components::icons::Mail;
+
+#[inline_props]
+pub fn GoogleMailThreadDisplay<'a>(
+    cx: Scope,
+    notif: &'a NotificationWithTask,
+    google_mail_thread: GoogleMailThread,
+) -> Element {
+    let is_starred = google_mail_thread.is_tagged_with(GOOGLE_MAIL_STARRED_LABEL, None);
+    let is_important = google_mail_thread.is_tagged_with(GOOGLE_MAIL_IMPORTANT_LABEL, None);
+    let from_address = google_mail_thread.get_message_header(MessageSelection::First, "From");
+    let interlocutors_count = google_mail_thread
+        .messages
+        .iter()
+        .fold(HashSet::new(), |mut acc, msg| {
+            if let Some(from_address) = msg.get_header("From") {
+                acc.insert(from_address);
+            }
+            acc
+        })
+        .len();
+    let mail_icon_style = match (is_starred, is_important) {
+        (_, true) => "text-red-500",
+        (true, false) => "text-yellow-500",
+        _ => "",
+    };
+
+    render! {
+        div {
+            class: "flex items-center gap-2",
+
+            Mail { class: "h-5 w-5 {mail_icon_style}" }
+
+            div {
+                class: "flex flex-col grow",
+
+                div {
+                    class: "flex flex-row items-center",
+                    span { class: "mx-0.5", "{notif.title}" }
+                    if is_starred {
+                        render! { Icon { class: "mx-0.5 h-3 w-3 text-yellow-500", icon: BsStar } }
+                    }
+                    if is_important {
+                        render! { Icon { class: "mx-0.5 h-3 w-3 text-red-500", icon: BsExclamationCircle } }
+                    }
+                }
+
+                div {
+                    class: "flex gap-2",
+
+                    if let Some(from_address) = from_address {
+                        render! { span { class: "text-xs text-gray-400", "{from_address}" } }
+                    }
+                    span { class: "text-xs text-gray-400", "({interlocutors_count})" }
+                }
+            }
+        }
+    }
+}
---
+++ web/src/components/integrations/todoist/notification.rs
@@ -0,0 +1,65 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use dioxus_free_icons::{
+    icons::bs_icons::{BsArrowRepeat, BsCalendar2Check, BsCardChecklist},
+    Icon,
+};
+
+use universal_inbox::{
+    notification::NotificationWithTask,
+    task::integrations::todoist::{TodoistItem, TodoistItemPriority},
+};
+
+#[inline_props]
+pub fn TodoistNotificationDisplay<'a>(
+    cx: Scope,
+    notif: &'a NotificationWithTask,
+    todoist_task: TodoistItem,
+) -> Element {
+    let title = markdown::to_html(&notif.title);
+    let task_icon_style = match todoist_task.priority {
+        TodoistItemPriority::P1 => "",
+        TodoistItemPriority::P2 => "text-yellow-500",
+        TodoistItemPriority::P3 => "text-orange-500",
+        TodoistItemPriority::P4 => "text-red-500",
+    };
+
+    render! {
+        div {
+            class: "flex items-center gap-2",
+
+            Icon { class: "h-5 w-5 {task_icon_style}", icon: BsCardChecklist }
+
+            div {
+                class: "flex flex-col grow",
+
+                span { dangerous_inner_html: "{title}" }
+                div {
+                    class: "flex gap-2",
+
+                    if let Some(due) = &todoist_task.due {
+                        render! {
+                            div {
+                                class: "flex items-center text-xs text-gray-400 gap-1",
+
+                                Icon { class: "h-3 w-3", icon: BsCalendar2Check }
+                                "{due.date}"
+                                if due.is_recurring {
+                                    render! { Icon { class: "h-3 w-3", icon: BsArrowRepeat } }
+                                }
+                            }
+                        }
+                    }
+
+                    div {
+                        class: "flex gap-2",
+                        for label in &todoist_task.labels {
+                            render! { span { class: "text-xs text-gray-400", "@{label}" } }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
